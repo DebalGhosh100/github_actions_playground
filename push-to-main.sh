ae83a32cfbf9a0dbabd4281cbe1b3d36060e44fa
DebalGhosh100 <debalghosh100@gmail.com>
2025-04-16 18:33:46 +0530

    AddedWorkflow

---
+++ push-to-main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if an argument was provided
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 \"commit message\""
+  exit 1
+fi
+
+# Store the argument as commit message
+commit_message="$1"
+
+# Run the git commands
+git checkout main
+git pull
+git add .
+git commit -m "$commit_message"
+git push
+
