6583727cf01cbea7e3a96ea76e04e4fddaf899f7
DebalGhosh100 <debalghosh100@gmail.com>
2025-04-22 14:05:08 +0530

    Added fail and succeed apps

---
+++ ghap/succeed_app/CMakeFiles/FailApp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FailApp.dir/main.cpp.o"
+  "FailApp"
+  "FailApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FailApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
